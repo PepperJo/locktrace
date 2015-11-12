f8ff79febe252fa496c7335f3944c87d5173abf7
Jonas Pfefferle <pepperjo@japf.ch>
2015-11-12 16:48:13 +0100

    introduce record lock limit per table per transaction

--- main.cpp
+++ main.cpp
@@ -10,8 +10,11 @@
 #include <boost/program_options.hpp>
 #include <boost/algorithm/string.hpp>
 
-enum class LockMode {
-    X, S, IX, IS
+enum LockMode {
+    X = 1,
+    S = 1<<1,
+    IX = 1<<2,
+    IS = 1<<3
 };
 
 inline std::istream& operator>>(std::istream& in, LockMode& mode) {
@@ -35,38 +38,46 @@ using LockKey = uint64_t;
 
 struct Lock {
     LockKey key;
+    std::string dbg_str;
     LockMode mode;
 };
 
+inline bool operator==(const Lock& a, const LockKey& key) {
+    return a.key == key;
+}
+
 struct Transaction {
     uint64_t id;
-    std::vector<Lock> lock;
-    std::vector<LockKey> unlock;
+    std::list<Lock> lock;
+    std::vector<decltype(lock)::iterator> unlock;
 };
 
 std::vector<Transaction> trxs;
 
-int parse_lock_log(std::istream& in) {
-    std::multimap<std::pair<size_t, size_t>, Lock> page_map;
-    std::set<LockKey> lock_set;
-    bool unlock = true;
+int parse_lock_log(std::istream& in, size_t record_lock_limit) {
+    std::multimap<size_t, decltype(Transaction::lock)::iterator> table_map;
+    bool lock = false;
     decltype(trxs)::iterator trx;
     while (in.good()) {
         std::string line;
         std::getline(in, line);
+        if (!line.size()) {
+            continue;
+        }
         std::deque<std::string> strs;
         boost::split(strs, line, boost::is_any_of(" "));
         LOG_ERR_EXIT(strs.size() < 9, EINVAL, std::system_category());
 
         if(strs[0] == "UNLOCK") {
-            unlock = true;
+            lock = false;
             strs.pop_front();
         } else {
-            if (unlock) {
+            if (!lock) {
                 /* 2PL -> started a new transaction! */
+                table_map.clear();
                 trx = trxs.insert(trxs.end(), Transaction{0});
             }
-            unlock = false;
+            lock = true;
         }
 
         bool record;
@@ -89,38 +100,67 @@ int parse_lock_log(std::istream& in) {
 
         std::hash<std::string> hashfn;
         LockKey key;
-        key = hashfn(record ? strs[3]+ strs[5] + strs[7] : strs[4]);
+        std::string dbg_str = record ? strs[3] + ' ' + strs[5] + ' ' + strs[7] : strs[3];
+        key = hashfn(dbg_str);
+        // std::cout << (record ? "record: " : "table: ") << key << '\n';
 
-        if (unlock) {
-            if (record) {
-                /* release all the record from this page! */
-                // page_map.find()
+        LockMode mode;
+        ss.clear();
+        ss.str(strs[record ? 17 : 8]);
+        ss >> mode;
+        LOG_ERR_EXIT(ss.fail(), EINVAL, std::system_category());
+
+        if (lock) {
+            if (std::find(trx->lock.crbegin(), trx->lock.crend(), key)
+                    == trx->lock.crend()) {
+                if (record) {
+                    size_t table_key = hashfn(strs[12]);
+                    auto it = std::find(trx->lock.begin(), trx->lock.end(), table_key);
+                    LOG_ERR_EXIT(it == trx->lock.end(), EINVAL, std::system_category());
+
+                    /* if we locked the table S or X we do not need to take the
+                     * record lock anymore (record_limit optimization) */
+                    if (it->mode & (LockMode::IS | LockMode::IX)) {
+                        auto table_range = table_map.equal_range(table_key);
+                        if (std::distance(table_range.first, table_range.second) > record_lock_limit) {
+                            /* 1. remove all records locks of this table
+                             * (including the on we just inserted)
+                             * 2. take table lock (S|X)! */
+                            for (auto to_delete = table_range.first; to_delete != table_range.second;) {
+                                trx->lock.erase(to_delete->second);
+                                to_delete = table_map.erase(to_delete);
+                            }
+                            it->mode = it->mode & LockMode::IX ? LockMode::X : LockMode::S;
+                         } else {
+                            auto new_lock = trx->lock.insert(trx->lock.end(), {key, dbg_str ,mode});
+                            table_map.insert({table_key, new_lock});
+                         }
+                    }
+                } else {
+                    trx->lock.push_back({key, dbg_str, mode});
+                }
             } else {
-                trx->unlock.push_back(key);
+                // TODO: std::cout << "upgrade?\n";
             }
-        } else {
-            Lock lock;
-            lock.key = key;
-            ss.clear();
-            ss.str(strs[record ? 17 : 8]);
-            ss >> lock.mode;
-            LOG_ERR_EXIT(ss.fail(), EINVAL, std::system_category());
-            bool inserted = lock_set.insert(key).second;
-            if (!inserted) {
-                /* do we need to upgrade the lock? */
-            }
-            trx->lock.push_back(lock);
-            if (record) {
-                size_t space, page_no;
-                ss.clear();
-                ss.str(strs[3]);
-                ss >> space;
-                LOG_ERR_EXIT(ss.fail(), EINVAL, std::system_category());
-                ss.clear();
-                ss.str(strs[5]);
-                ss >> page_no;
-                LOG_ERR_EXIT(ss.fail(), EINVAL, std::system_category());
-                page_map.insert({{space, page_no}, lock});
+        } else if (!record) { /* unlock table (unlocks also record locks) */
+            /* ASSUMPTION: SS2PL, i.e. all locks are released after commit:
+             * 1) No unlock order
+             * 2) locks are not downgraded (X -> S) etc. always unlock in one go */
+
+            auto it = std::find(trx->lock.begin(), trx->lock.end(), key);
+            LOG_ERR_EXIT(it == trx->lock.end(), EINVAL, std::system_category());
+            if (std::find(trx->unlock.begin(), trx->unlock.end(), it)
+                    == trx->unlock.end()) {
+                /* release all locks belonging to this table */
+                auto table_range = table_map.equal_range(key);
+                if (table_range.first != table_range.second) {
+                    table_range.second--;
+                    for (auto l = table_range.second; l != table_range.first; l--) {
+                        trx->unlock.emplace_back(l->second);
+                    }
+                    trx->unlock.emplace_back(table_range.first->second);
+                }
+                trx->unlock.emplace_back(it);
             }
         }
     }
@@ -136,7 +176,9 @@ int main(int argc, char* argv[]) {
         ("ip", bop::value<psl::net::in_addr>()->required(), "server ip")
         ("p", bop::value<psl::net::in_port_t>()->default_value(1234), "server port")
         ("l", bop::value<std::string>()->required(), "lock log-file")
-        ("q", bop::value<std::string>(), "query log-file");
+        ("q", bop::value<std::string>(), "query log-file")
+        ("limit", bop::value<size_t>()->default_value(std::numeric_limits<size_t>::max()),
+         "transaction record lock limit -> upgrade to table lock if reached");
 
     bop::variables_map vm;
     bop::store(
@@ -158,7 +200,29 @@ int main(int argc, char* argv[]) {
     LOG_ERR_EXIT(!lock_log.good(), EINVAL, std::system_category());
 
     int ret;
-    LOG_ERR_EXIT((ret = parse_lock_log(lock_log)), ret, std::system_category());
+    LOG_ERR_EXIT((ret = parse_lock_log(lock_log, vm["limit"].as<size_t>())), ret,
+            std::system_category());
+
+    uint64_t n_locks_avg = 0;
+    uint32_t nn = 0;
+    for (auto& trx : trxs) {
+        std::cout << "---------------------------------------------------\n";
+        std::cout << "Transaction " << trx.id << '\n';
+        std::cout << "---------------------------------------------------\n";
+        std::cout << "n_locks: " << trx.lock.size() << '\n';
+        n_locks_avg += trx.lock.size();
+        std::cout << "n_unlocks: " << trx.unlock.size() << '\n';
+        for (auto& l : trx.lock) {
+            std::cout << "LOCK " << l.dbg_str << " ["
+                << static_cast<uint32_t>(l.mode) << "]\n";
+        }
+        for (auto ul : trx.unlock) {
+            std::cout << "UNLOCK " << ul->dbg_str << '\n';
+        }
+        nn++;
+    }
+
+    std::cout << "lock_avg: " << n_locks_avg / static_cast<double>(nn) << '\n';
 
     return 0;
 }
